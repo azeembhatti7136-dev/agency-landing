7e61b694cd081cfbaec86487248ef211e4045e32
azeembhatti7136-dev <azeembhatti7136@gmail.com>
2026-02-05 20:33:27 +0500

    Update page.js

--- app/page.js
+++ app/page.js
@@ -24,7 +24,8 @@ async function getAgencyData() {
   });
 
   try {
-   const baseUrl = process.env.NEXT_PUBLIC_STRAPI_URL || "https://agency-backend-production-270b.up.railway.app";
+  const baseUrl = "https://agency-backend-production-270b.up.railway.app";
+
 const res = await fetch(`${baseUrl}/api/agency-page?${query.toString()}`, { 
   cache: 'no-store' 
 });
